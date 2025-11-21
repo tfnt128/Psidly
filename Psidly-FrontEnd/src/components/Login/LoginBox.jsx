e92b66496d8940bc57220eaa39a4d4cab2e12693
andredev <andluccag@gmail.com>
2025-11-21 10:05:42 -0300

    Commit previo dos inputs do login

--- Psidly-FrontEnd/src/components/Login/LoginBox.jsx
+++ Psidly-FrontEnd/src/components/Login/LoginBox.jsx
@@ -1,7 +1,17 @@
-export default function LoginBox({Style}){
+import PessoaCerebro from "../../assets/icons/pessoacerebro.png"
+import Input from "../General/Input"
+
+export default function LoginBox(){
 
     return(
-        <>
-        </>
+        <div className="flex flex-col items-center mt-20 bg-terciario rounded-[50px] w-[550px] h-[630px]">
+            <h1 className="font-lexenddeca color-terciario text-[30px] mt-4">Login</h1>
+            <img src={PessoaCerebro} className="w-[250px] -ml-10 -mt-5"/>
+            <Input Style={"w-[80%] bg-primario p-[25px] rounded-[15px] placeholder:text-[19px] placeholder:font-lexenddeca"} PlaceHolder={'Insira seu e-mail'}/>
+            <Input Style={"w-[80%] bg-primario p-[25px] rounded-[15px] placeholder:text-[19px] placeholder:font-lexenddeca"} PlaceHolder={'Insira seu e-mail'}/>
+
+
+
+        </div>
     )
 }
--- Psidly-FrontEnd/src/pages/LoginPages/Login.jsx
+++ Psidly-FrontEnd/src/pages/LoginPages/Login.jsx
@@ -1,9 +1,11 @@
+import LoginBox from "../../components/Login/LoginBox"
 import Title from "../../components/Titles/Title"
 
 export default function Login(){
     return(
         <div className="w-full h-screen bg-linear-to-b from-[#ffffff] to-[#9AC8FF] flex flex-col items-center">
-            <Title Style={'w-[30%] lg:w-[10%]'}/>
+            <Title Style={'w-[25%] lg:w-[10%]'}/>
+            <LoginBox/>
         </div>
 
     )
