e38b0da11367435f514004573e8ce1e2b40b7070
andredev <andluccag@gmail.com>
2025-11-23 22:45:49 -0300

    Commit

--- Psidly-FrontEnd/src/pages/Cadastro/Cadastropage.jsx
+++ Psidly-FrontEnd/src/pages/Cadastro/Cadastropage.jsx
@@ -6,14 +6,14 @@ export default function Cadastropage(){
     return(
         <>
             <div className=" flex lg:hidden">
-                <div className="w-full h-screen bg-linear-to-b from-[#ffffff] to-[#9AC8FF] flex flex-col items-center">
+                <div className="w-full h-screen overflow-y-auto  bg-linear-to-b from-[#ffffff] to-[#9AC8FF] flex flex-col items-center">
                     <Title Style={'w-[25%] lg:w-[20%]'}/>
                     <CadastroBox/>
                 </div>
 
             </div>
             <div className="hidden lg:flex">
-                <div className="w-[50%] h-screen bg-linear-to-b from-[#ffffff] to-[#9AC8FF] flex flex-col items-center">
+                <div className="w-[50%] h-screen overflow-y-auto  bg-linear-to-b from-[#ffffff] to-[#9AC8FF] flex flex-col items-center">
                     <Title Style={'w-[25%] lg:w-[20%]'}/>
                     <CadastroBox/>
                 </div>
