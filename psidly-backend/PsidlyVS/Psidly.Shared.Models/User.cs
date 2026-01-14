c9547cbadd5c7988beca9c6876873b26fccd1277
tfnt128 <thiago.fontoura120@gmail.com>
2026-01-14 20:42:57 -0300

    v0.1

---
+++ psidly-backend/PsidlyVS/Psidly.Shared.Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psidly.Shared.Models.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Crp { get; set; }
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? PasswordHash { get; set; }
+
+        public string? ResetPasswordCode { get; set; }
+        public DateTime? ResetPasswordCodeExpiry { get; set; }
+    }
+
+}
