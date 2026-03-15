64aa821d160083573b7142f83a45e58c4ce8f8e2
andredev <andluccag@gmail.com>
2026-03-15 19:18:02 -0300

    Commit inicial de adc novo paciente

---
+++ src/components/Homepage/NewPatient.jsx
@@ -0,0 +1,13 @@
+export default function NewPatient(){
+
+
+
+    return(
+        <>
+            <div className="bg-blue-600 h-[70%] w-[40%] lg:hidden flex flex-col items-center rounded-[30px]">
+                <h1 className="font-lexenddeca text-[60px]">Novo paciente</h1>
+            </div>
+        </>
+
+    )
+}
