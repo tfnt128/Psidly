4fb9734a277da1f7b057b3826192277bdc2afcd5
andredev <andluccag@gmail.com>
2026-03-15 19:32:25 -0300

    COmmit previo

--- src/components/Homepage/AddButton.jsx
+++ src/components/Homepage/AddButton.jsx
@@ -1,7 +1,9 @@
 import Button from "../General/Button";
 
-export default function AddButton(){
+export default function AddButton({onClickFunction}){
     return(
-        <Button Style={"rounded-full h-[100px] w-[100px] lg:h-[300px] lg:w-[300px] bg-secundario text-[40px] lg:text-[120px] flex items-center justify-center font-lexenddeca"}  Text={"+"}/>
+        <Button Style={"rounded-full h-[100px] w-[100px] lg:h-[300px] lg:w-[300px] bg-secundario text-[40px] lg:text-[120px] flex items-center justify-center font-lexenddeca"}  Text={"+"}
+            OnClickFunction={onClickFunction}
+        />
     )
 }
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import AddButton from "../../components/Homepage/AddButton";
 import Homeheader from "../../components/Homepage/Homeheader";
 import Homemenu from "../../components/Homepage/Homemenu";
 import Homemenuaside from "../../components/Homepage/Homemenuaside";
 
 export default function Homepage(){
+
+    const [screenBlur, setScreenBlur] = useState(false)
+    function openNewPatientModal(){
+        setScreenBlur(true)
+    }
+
     return(
+        
         <div className="min-h-screen flex flex-col">
+            {
+                screenBlur &&
+                    <div className="fixed inset-0 backdrop-blur-sm z-40 min-h-screen min-w-screen"
+                    onClick={() => setScreenBlur(false)}>
+
+
+                    </div>
+            }
             <Homeheader/>
 
             <div className="fixed  bottom-[120px] lg:right-30 right-4 ">
-                <AddButton/>
+                <AddButton onClickFunction={openNewPatientModal}/>
             </div>
 
             <div className="fixed lg:hidden bottom-0 left-0 w-full ">
