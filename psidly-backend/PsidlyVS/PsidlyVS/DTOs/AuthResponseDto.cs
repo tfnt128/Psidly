16343123960c190b1823a4a2f4077b2087b17c5f
tfnt128 <thiago.fontoura120@gmail.com>
2026-05-02 12:39:03 -0300

    direferenca paciente e psicologo

--- psidly-backend/PsidlyVS/PsidlyVS/DTOs/AuthResponseDto.cs
+++ psidly-backend/PsidlyVS/PsidlyVS/DTOs/AuthResponseDto.cs
@@ -4,8 +4,9 @@
     {
         public bool Success { get; set; }
         public string? Message { get; set; }
-        public UserDto? User { get; set; }
+        public Object? User { get; set; }
         public string? Token { get; set; }
+        public string? UserType { get; set; }      
     }
 
     public class UserDto
