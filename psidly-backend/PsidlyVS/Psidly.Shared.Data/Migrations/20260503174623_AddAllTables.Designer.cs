80c9c83c978dcb4f4baa1417ad47a247066b6fef
Nicolas Gimenez Alcalde <nicolasgimenez.al@gmail.com>
2026-05-03 21:42:36 -0300

    Criacao de todos os models

--- psidly-backend/PsidlyVS/Psidly.Shared.Data/Migrations/20260503174623_AddAllTables.Designer.cs
+++ psidly-backend/PsidlyVS/Psidly.Shared.Data/Migrations/20260503174623_AddAllTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Psidly.Shared.Data.Data;
@@ -11,9 +12,11 @@ using Psidly.Shared.Data.Data;
 namespace Psidly.Shared.Data.Migrations
 {
     [DbContext(typeof(PsidlyContext))]
-    partial class PsidlyContextModelSnapshot : ModelSnapshot
+    [Migration("20260503174623_AddAllTables")]
+    partial class AddAllTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
